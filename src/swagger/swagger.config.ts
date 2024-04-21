2cb999ec13831599b93091a4cdb54246970dabcc
LBenzahia <lakhdar.benzahia@gmail.com>
2024-04-21 10:37:48 +0200

    added swagger module

---
+++ src/swagger/swagger.config.ts
@@ -0,0 +1,12 @@
+import { SwaggerConfig } from './swagger.interface';
+
+/**
+ * Configuration for the swagger UI (found at /api).
+ */
+export const SWAGGER_CONFIG: SwaggerConfig = {
+  title: 'air quality api',
+  description:
+    'A rest backend services online documentation for air quality api',
+  version: '1.0',
+  tags: ['air quality api'],
+};
